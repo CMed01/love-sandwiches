aed545ef62e365638e65e1f607e8f9b4f5ea2506
Christopher Meddings <c.m.meddings@gmail.com>
2023-07-06 15:34:41 +0000

    Display stock required for next market day to terminal

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT= gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches_walkthrough')
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
@@ -130,7 +130,7 @@ def calculate_stock_data(data):
         add_10_percent = round((average * 1.1))
         new_stock_data.append(add_10_percent)
     return new_stock_data
-      
+
 
 def main():
     """
@@ -139,14 +139,38 @@ def main():
     data = get_sales_data() 
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
+
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches data automation")
 print("---------------------------------------\n")
 
-main()
+stock_data = main()
+
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    # headings = SHEET.worksheet('stock').row_values(1)
+
+    print("Make the following numbers of sandwiches for next market:\n")
+
+    # new_data = {}
+    # for heading, stock_num in zip(headings, data):
+    #     new_data[heading] = stock_num
+    # return new_data
+    
+    return {heading: data for heading, data in zip(headings, data)}
+    
+stock_values = get_stock_values(stock_data)
+print(stock_values)
+
